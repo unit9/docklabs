799a41b0bc6602fa6aae558184ccee9ed1377b8f
Kamil Cholewiński <harry666t@gmail.com>
2016-12-21 13:29:42 +0100

    Some cool new endpoints
    - /: hello, version
    - /containers: list containers
    - /containers/<name>: inspect container
    - /whoami: matches client's remote address to container and inspects

--- inspector/main.py
+++ inspector/main.py
@@ -8,11 +8,13 @@ import sys
 import urlparse
 import wsgiref.handlers
 
-import werkzeug
-import werkzeug.serving
+import flask
 
 
-logging.basicConfig(level=logging.DEBUG)
+app = flask.Flask(__name__)
+app.debug = int(os.environ.get("DEBUG", "0"))
+app.logger.setLevel(logging.DEBUG)
+app.logger.addHandler(logging.StreamHandler())
 
 
 def getoutput(cmd):
@@ -29,6 +31,37 @@ def getoutput(cmd):
     return out
 
 
+def docker_list_containers():
+    lines = getoutput([
+        "docker", "ps", "-a",
+        "--format", "{{.ID}}\x1f{{.Names}}\x1f{{.Image}}",
+    ]).strip().split("\n")
+    data = [line.split("\x1f", 2) for line in lines]
+    return [{"id": id, "names": names.split(","), "image": image}
+                   for id, names, image in data]
+
+
+def docker_inspect_containers(*containers):
+    try:
+        return json.loads(getoutput([
+            "docker", "inspect", "--type", "container",
+        ] + list(containers)))
+    except RuntimeError as e:
+        if e.args[0] == 1:
+            return []
+        raise
+
+
+def docker_inspect_image_tags(image):
+    rawtags = getoutput([
+        "docker", "inspect", "--type", "image",
+        "-f", r'{{ range .RepoTags }}{{ printf "%s\n" .}}{{ end }}',
+        image,
+    ]).strip().split()
+    return [{"repository": repository, "tag": tag}
+            for repository, tag in map(lambda s: s.split(":"), rawtags)]
+
+
 def response(status=200, headers=None, data=None, message=None):
     if data is None:
         data = {}
@@ -43,39 +76,57 @@ def response(status=200, headers=None, data=None, message=None):
     )
 
 
-@werkzeug.wrappers.Request.application
-def application(request):
-    if request.path != "/":
-        return response(404)
-    if request.method != "GET":
-        return response(405)
-    query = dict(urlparse.parse_qsl(request.query_string))
-    try:
-        container = query["container"]
-    except LookupError:
-        return response(400, message="required parameter: container")
-    try:
-        image = getoutput([
-            "docker", "inspect", "--type", "container",
-            "-f", "{{.Config.Image}}", container,
-        ]).strip()
-    except RuntimeError as e:
-        if e.args[0] == 1:
-            return response(404)
-        raise
-    tags = getoutput([
-        "docker", "inspect", "--type", "image",
-        "-f", r'{{ range .RepoTags }}{{ printf "%s\n" .}}{{ end }}',
-        image,
+@app.route("/")
+def index():
+    client, server = getoutput([
+        "docker", "version",
+        "-f", "{{ .Client.Version }} {{ .Server.Version }}",
     ]).strip().split()
-    return response(data={"image": image, "tags": tags})
+    return flask.jsonify(
+        message="Hello",
+        version=dict(client=client, server=server),
+    )
+
+
+@app.route("/whoami")
+def get_whoami():
+    ip = flask.request.remote_addr
+    containers = docker_list_containers()
+    name = None
+    image = None
+    tags = []
+    for container in containers:
+        for container in docker_inspect_containers(*container["names"]):
+            if container["NetworkSettings"]["IPAddress"] == ip:
+                name = container["Name"].lstrip("/")
+                image = container["Config"]["Image"]
+                tags = docker_inspect_image_tags(image)
+                break
+    return flask.jsonify(ip=ip, name=name, image=image, tags=tags)
+
+
+@app.route("/containers")
+def list_containers():
+    containers = docker_list_containers()
+    return flask.jsonify(containers=containers)
+
+
+@app.route("/containers/<string:container>")
+def get_container(container):
+    containers = docker_inspect_containers(container)
+    if not containers:
+        return flask.abort(404)
+    image = containers[0]["Config"]["Image"]
+    tags = docker_inspect_image_tags(image)
+    return flask.jsonify(image=image, tags=tags, name=container)
 
 
 if __name__ == "__main__":
     if os.environ.get("REQUEST_METHOD"):
-        wsgiref.handlers.CGIHandler().run(application)
+        wsgiref.handlers.CGIHandler().run(app.wsgi_app)
     else:
+        import werkzeug.serving
         werkzeug.serving.run_simple(
             os.environ.get("HOST", "localhost"),
             int(os.environ.get("PORT", "8600")),
-            application)
+            app.wsgi_app)
